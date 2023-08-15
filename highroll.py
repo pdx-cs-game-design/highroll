245b377fe8712299b58af54f610a6e74ed16266b
Bart Massey <bart.massey@gmail.com>
2023-08-15 13:15:47 -0700

    fixed one bug, left another

--- highroll.py
+++ highroll.py
@@ -1,24 +1,33 @@
-def p(target, pp):
-    t1 = 0
-    for r in range(target+1, 7):
-        t1 += 1 - pp[r]
-    t1 /= 6
+# Note: the reasoning isn't currently right here. The
+# first player must beat *both* the second and third players
+# to win. This calculation only accounts for the first player beating
+# the second player.
 
-    t2 = 0
-    for r in range(1, target+1):
-        t2r = 0
+# Probability of winning given a target score to beat
+# and a matrix of probabilities that a subsequent player
+# will win.
+def p(target, pp):
+    pt = 0
+    for r in range(1, 7):
+        # Probability of winning when stopping after first roll of r.
+        p1 = int(r > target) * (1 - pp[r])
+
+        # Probability of winning on second roll after first roll of r.
+        p2 = 0
         for s in range(1, 7):
-            t2r += int(target < r + s and r + s <= 6) * (1 - pp[s])
-        t2r /= 6
-        t2 += t2r
-    t2 /= max(target, 1)
-    return t1 + (1 - t1) * t2
+            if r + s > 6:
+                continue
+            p2 += int(target < r + s) * (1 - pp[r + s]) / 6
+
+        pt += max(p1, p2) / 6
+
+    return pt
 
 ps = dict()
 ps[4] = {i : 0 for i in range(1, 7)}
 ps[3] = {target : p(target, ps[4]) for target in range(1, 7)}
 ps[2] = {target : p(target, ps[3]) for target in range(1, 7)}
-ps[1] = {target : p(0, ps[2]) for target in range(1, 7)}
+ps[1] = p(0, ps[2])
 
 for player in range(1, 4):
     print(ps[player])
